0e415dc28270367367819df214ffed3e8eb37621
Shailendra Singh <119068680+ShailendraSingh0009@users.noreply.github.com>
2023-06-24 14:46:16 +0530

    Add files via upload

---
+++ Signup_project/myapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect,HttpResponse
+from .forms import signupform
+from django.contrib.auth import authenticate,login,logout
+
+# Create your views here.
+
+def signupview(request):
+	if request.method=='POST':
+		form = signupform(request.POST)
+		if form.is_valid():
+			form.save
+			return redirect('login')
+
+	else:
+	     form = signupform()		
+	return render(request,'signup.html',{'form':form})
+
+
+def Login(request):
+	if request.method=="POST":
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(username=username,password=password)
+		if user is not None:
+			login(request,user)
+			return render('next')
+	else:		
+	     return render(request,'login.html')
+
+def Next(request):
+	return render(request,'next.html')
+
+
+
+
+
